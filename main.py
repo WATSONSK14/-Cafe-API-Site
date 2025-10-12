648e66f22c24c99d8173617c1dbf295526665189
WATSONSK14 <serhatkutlu14@gmail.com>
2025-10-13 00:07:51 +0300

    Fix port binding for Render deployment

--- main.py
+++ main.py
@@ -33,4 +33,5 @@ with app.app_context():
 
 
 if '__main__' == __name__:
-    app.run(debug=app.config['DEBUG'])
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port, debug=app.config['DEBUG'])
